66cb0ca34290b50f47c883e3a68526f5174baafe
[derekj-s] <[serrand@mcmaster.ca]>
2020-10-06 02:56:08 -0400

    handling collisions

--- src/hash_table.c
+++ src/hash_table.c
@@ -56,3 +56,11 @@ static int ht_hash(const char *s, const int a, const int m){
 	}
 	return (int)hash;
 }
+
+static int ht_get_hash(
+    const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s,11, num_buckets);
+    const int hash_b = ht_hash(s, 7, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
